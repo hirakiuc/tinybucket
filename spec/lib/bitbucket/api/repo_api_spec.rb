72a5270ff0ec8f962588f275e16b0e17fdad2d3e
hirakiuc <hirakiuc@gmail.com>
2014-07-09 06:19:26 +0900

    add api/repo_api_spec.rb

--- spec/lib/bitbucket/api/repo_api_spec.rb
+++ spec/lib/bitbucket/api/repo_api_spec.rb
@@ -8,13 +8,19 @@ RSpec.describe Bitbucket::Api::RepoApi do
 
   it { expect(api).to be_a_kind_of(Bitbucket::Api::BaseApi) }
 
+  let(:options){ {} }
+
+  let(:owner){ nil }
+  let(:repo_slug){ nil }
+  let(:request_path){ nil }
+  before do
+    api.owner = owner
+    api.repo_slug = repo_slug
+    stub_apiresponse(:get, request_path) if request_path.present?
+  end
+
   describe 'find' do
-    let(:options){ {} }
     subject{ api.find(options) }
-    before do
-      api.owner = owner
-      api.repo_slug = repo_slug
-    end
 
     context 'when without owner and repo_slug' do
       let(:owner){ nil }
@@ -23,33 +29,50 @@ RSpec.describe Bitbucket::Api::RepoApi do
     end
 
     context 'when without owner' do
-      pending
+      let(:owner){ nil }
+      let(:repo_slut){ 'test_repo' }
+      it { expect{ subject }.to raise_error(ArgumentError) }
     end
 
     context 'when without repo_slug' do
-      pending
+      let(:owner){ 'test_owner' }
+      let(:repo_slug){ nil }
+      it { expect{ subject }.to raise_error(ArgumentError) }
     end
 
     context 'when with owner and repo_slug' do
-      pending
+      let(:owner){ 'test_owner' }
+      let(:repo_slug){ 'test_repo' }
+      let(:request_path){ '/repositories/test_owner/test_repo' }
+      it { expect(subject).to be_an_instance_of(Bitbucket::Models::Repository) }
     end
   end
 
   describe 'path_to_find' do
+    subject{ api.send(:path_to_find) }
+
     context 'when without owner and repo_slug' do
-      pending
+      let(:owner){ nil }
+      let(:repo_slug){ nil }
+      it { expect{ subject }.to raise_error(ArgumentError) }
     end
 
     context 'when without owner' do
-      pending
+      let(:owner){ nil }
+      let(:repo_slug){ 'test_repo' }
+      it { expect{ subject }.to raise_error(ArgumentError) }
     end
 
     context 'when without repo_slug' do
-      pending
+      let(:owner){ 'test_owner' }
+      let(:repo_slug){ nil }
+      it { expect{ subject }.to raise_error(ArgumentError) }
     end
 
     context 'when with owner and repo_slug' do
-      pending
+      let(:owner){ 'test_owner' }
+      let(:repo_slug){ 'test_repo' }
+      it { expect(subject).to eq('/repositories/test_owner/test_repo') }
     end
   end
 end
