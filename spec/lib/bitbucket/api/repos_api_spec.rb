c9c8cafafdc6655f85dd9676857cabbad529e3b4
hirakiuc <hirakiuc@gmail.com>
2014-08-04 23:47:27 +0900

    remove find method spec.

--- spec/lib/bitbucket/api/repos_api_spec.rb
+++ spec/lib/bitbucket/api/repos_api_spec.rb
@@ -25,35 +25,4 @@ RSpec.describe Bitbucket::Api::ReposApi do
       it { expect(subject).to be_an_instance_of(Bitbucket::Models::Page) }
     end
   end
-
-  describe 'find' do
-    let(:repo_owner) { 'test_owner' }
-    let(:repo_slug) { 'test_repo' }
-    let(:request_path) { nil }
-
-    subject { api.find(repo_owner, repo_slug) }
-
-    before { stub_apiresponse(:get, request_path) if request_path }
-
-    context 'when without repo_owner and repo_slug' do
-      let(:repo_owner) { nil }
-      let(:repo_slug) { nil }
-      it { expect { subject }.to raise_error(ArgumentError) }
-    end
-
-    context 'when without repo_owner' do
-      let(:repo_owner) { nil }
-      it { expect { subject }.to raise_error(ArgumentError) }
-    end
-
-    context 'when without repo_slug' do
-      let(:repo_slug) { nil }
-      it { expect { subject }.to raise_error(ArgumentError) }
-    end
-
-    context 'when with repo_owner and repo_slug' do
-      let(:request_path) { "/repositories/#{repo_owner}/#{repo_slug}" }
-      it { expect(subject).to be_an_instance_of(Bitbucket::Models::Repository) }
-    end
-  end
 end
