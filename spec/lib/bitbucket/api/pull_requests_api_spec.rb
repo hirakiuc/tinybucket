d26313496490118f63bf327cb01a2136046ce85c
hirakiuc <hirakiuc@gmail.com>
2014-08-04 17:04:30 +0900

    add find method spec.

--- spec/lib/bitbucket/api/pull_requests_api_spec.rb
+++ spec/lib/bitbucket/api/pull_requests_api_spec.rb
@@ -3,68 +3,100 @@ require 'spec_helper.rb'
 RSpec.describe Bitbucket::Api::PullRequestsApi do
   include ApiResponseMacros
 
-  let(:api_config){ {} }
-  let(:api){ Bitbucket::Api::PullRequestsApi.new(api_config) }
+  let(:api_config) { {} }
+  let(:api) { Bitbucket::Api::PullRequestsApi.new(api_config) }
+
+  let(:owner) { 'test_owner' }
+  let(:slug) { 'test_repo' }
+  let(:request_path) { nil }
 
   it { expect(api).to be_a_kind_of(Bitbucket::Api::PullRequestsApi) }
 
-  describe 'list' do
-    let(:request_path){ nil }
-    let(:options){ {} }
-    subject{ api.list(options) }
+  before do
+    api.repo_owner = owner
+    api.repo_slug = slug
+    stub_apiresponse(:get, request_path) if request_path
+  end
 
-    before do
-      api.repo_owner = repo_owner
-      api.repo_slug  = repo_slug
-      stub_apiresponse(:get, request_path) if request_path.present?
-    end
+  describe 'list' do
+    let(:options) { {} }
+
+    subject { api.list(options) }
 
     context 'without repo_owner and repo_slug' do
-      let(:repo_owner){ nil }
-      let(:repo_slug){ nil }
-      it { expect{ subject }.to raise_error(ArgumentError) }
+      let(:owner) { nil }
+      let(:slug) { nil }
+      it { expect { subject }.to raise_error(ArgumentError) }
     end
 
     context 'without repo_owner' do
-      let(:repo_owner){ nil }
-      let(:repo_slug){ 'test_repo' }
-      it { expect{ subject }.to raise_error(ArgumentError) }
+      let(:owner) { nil }
+      let(:slug) { 'test_repo' }
+      it { expect { subject }.to raise_error(ArgumentError) }
     end
 
     context 'without repo_slug' do
-      let(:repo_owner){ 'test_owner' }
-      let(:repo_slug){ nil }
-      it { expect{ subject }.to raise_error(ArgumentError) }
+      let(:owner) { 'test_owner' }
+      let(:slug) { nil }
+      it { expect { subject }.to raise_error(ArgumentError) }
     end
 
     context 'with repo_owner and repo_slug' do
-      let(:repo_owner){ 'test_owner' }
-      let(:repo_slug){ 'test_repo' }
-      let(:request_path){ '/repositories/test_owner/test_repo/pullrequests' }
+      let(:request_path) { "/repositories/#{owner}/#{slug}/pullrequests" }
       it { expect(subject).to be_an_instance_of(Bitbucket::Models::Page) }
     end
 
     context 'when request with state' do
-      let(:repo_owner){ 'test_owner' }
-      let(:repo_slug){ 'test_repo' }
-      let(:options){ { state: state } }
-      let(:request_path){ "/repositories/test_owner/test_repo/pullrequests?state=#{state}" }
+      let(:options) { { state: state } }
+      let(:request_path) do
+        "/repositories/#{owner}/#{slug}/pullrequests?state=#{state}"
+      end
 
       context 'when state is OPEN' do
-        let(:state){ 'OPEN' }
+        let(:state) { 'OPEN' }
         it { expect(subject).to be_an_instance_of(Bitbucket::Models::Page) }
       end
 
       context 'when state is DECLINED' do
-        let(:state){ 'DECLINED' }
+        let(:state) { 'DECLINED' }
         it { expect(subject).to be_an_instance_of(Bitbucket::Models::Page) }
       end
 
       context 'when state is MERGED' do
-        let(:state){ 'MERGED' }
+        let(:state) { 'MERGED' }
         it { expect(subject).to be_an_instance_of(Bitbucket::Models::Page) }
       end
     end
+  end
 
+  describe 'find' do
+    let(:pr_id) { 1 }
+    subject { api.find(pr_id) }
+
+    context 'when without repo_owner and repo_slug' do
+      let(:owner) { nil }
+      let(:slug) { nil }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context 'when without repo_owner' do
+      let(:owner) { nil }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context 'when without repo_slug' do
+      let(:slug) { nil }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context 'when with repo_owner and repo_slug' do
+      let(:request_path) do
+        "/repositories/#{owner}/#{slug}/pullrequests/#{pr_id}"
+      end
+
+      it 'return pull request model' do
+        expect(subject).to be_an_instance_of(Bitbucket::Models::PullRequest)
+      end
+    end
   end
 end
