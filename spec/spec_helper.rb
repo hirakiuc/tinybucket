25556601e79dad7c1e7cc665cc4638bb2d503b78
hirakiuc <hirakiuc@gmail.com>
2014-08-10 20:38:09 +0900

    mkdir log if not exist.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,11 +25,12 @@ path = Pathname.new(Dir.pwd)
 Dir[path.join('spec/support/**/*.rb')].each { |f| require f }
 
 # configure tinybucket logger.
+Dir.mkdir('log') unless Dir.exist?('log')
+
 logger = Logger.new('log/test.log')
 logger.level = Logger::DEBUG
 Tinybucket.logger = logger
 
 RSpec.configure do |config|
-
   config.order = 'random'
 end
