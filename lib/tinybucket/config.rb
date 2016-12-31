5207b290afea5367c3868b8a115f92a1b589b133
hirakiuc <hirakiuc@gmail.com>
2016-12-31 20:11:26 +0900

    :sparkles: Support OAuth2 (#106)

--- lib/tinybucket/config.rb
+++ lib/tinybucket/config.rb
@@ -3,6 +3,7 @@
 module Tinybucket
   class Config
     include ActiveSupport::Configurable
-    config_accessor :logger, :oauth_token, :oauth_secret, :cache_store_options
+    config_accessor :logger, :oauth_token, :oauth_secret,
+                    :access_token, :cache_store_options
   end
 end
