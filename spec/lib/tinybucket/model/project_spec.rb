e8efbef6e72fbd00bedad5b66202b78ccb64c956
hirakiuc <hirakiuc@gmail.com>
2017-07-15 16:28:31 +0900

    :sparkles: Support project repos api

--- spec/lib/tinybucket/model/project_spec.rb
+++ spec/lib/tinybucket/model/project_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe Tinybucket::Model::Project do
     subject { instance.destroy() }
     it { expect { subject }.to raise_error(NotImplementedError) }
   end
+
+  describe '#repos' do
+    subject { instance.repos }
+
+    context 'with loaded instance' do
+      let(:owner) { 'test_team' }
+      let(:request_path) { "/repositories/#{owner}?q='project.key=myprj'" }
+      before { stub_apiresponse(:get, request_path) }
+      it { expect(subject).to be_an_instance_of(Tinybucket::Resource::Repos) }
+    end
+
+    context 'with the instance which does not have owner property' do
+      let(:instance) { Tinybucket::Model::Project.new({}) }
+
+      it { expect { subject }.to raise_error('This project is not loaded yet.') }
+    end
+  end
 end
