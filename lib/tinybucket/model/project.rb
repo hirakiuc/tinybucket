01562c19aa725eeaafe82d1a4c2cc0b7d8c9339f
José María López Palomo <jose@iJose13.local>
2019-01-22 13:02:10 +0100

    Project methods update and destroy implmented with tests provided

--- lib/tinybucket/model/project.rb
+++ lib/tinybucket/model/project.rb
@@ -3,23 +3,49 @@
 module Tinybucket
   module Model
     class Project < Base
+      # Project
+      #
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/teams/%7Busername%7D/projects
+      #   projects Endpoint
+      #
+      # @!attribute [rw] type
+      #   @return [String]
+      # @!attribute [rw] description
+      #   @return [String, NilClass]
+      # @!attribute [rw] links
+      #   @return [Hash]
+      # @!attribute [rw] uuid
+      #   @return [String]
+      # @!attribute [rw] created_on
+      #   @return [String]
+      # @!attribute [rw] key
+      #   @return [String]
+      # @!attribute [rw] updated_on
+      #   @return [String]
+      # @!attribute [rw] is_private
+      #   @return [TrueClass, FalseClass]
+      # @!attribute [rw] name
+      #   @return [String]
+      # @!attribute [rw] owner
+      #   @return [Hash]
       acceptable_attributes \
         :type, :description, :links, :uuid, :created_on,
         :key, :updated_on, :is_private, :name, :owner
 
-      # Update this project
+      # Update the remote reference and the instance of this project
       #
-      # @param _params [Hash]
-      # @raise [NotImplementedError] to be implemented
-      def update(_params)
-        raise NotImplementedError
+      # @param options [Hash]
+      # @return [Tinybucket::Model::Project]
+      def update(options)
+        self.attributes = projects_api.put(key, options).attributes
+        self
       end
 
       # Destroy this project
       #
-      # @raise [NotImplementedError] to be implemented.
+      # @return [NilClass]
       def destroy
-        raise NotImplementedError
+        projects_api.delete(key)
       end
 
       # Get repositories
@@ -30,6 +56,12 @@ module Tinybucket
       end
 
       private
+
+      def projects_api
+        create_api('Projects').tap do |api|
+          api.owner = owner["username"]
+        end
+      end
 
       def owner_name
         raise 'This project is not loaded yet.' if (owner.nil? || owner['username'].nil?)
--- spec/lib/tinybucket/model/project_spec.rb
+++ spec/lib/tinybucket/model/project_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Tinybucket::Model::Project do
   include ApiResponseMacros
 
   let(:model_json) { load_json_fixture('teams/test_team/projects/myprj/get') }
+  let(:owner) { model_json["owner"]["username"] }
+  let(:project_key) { model_json["name"] }
   let(:instance) { Tinybucket::Model::Project.new(model_json) }
 
   it_behaves_like 'model has acceptable_attributes',
@@ -11,13 +13,19 @@ RSpec.describe Tinybucket::Model::Project do
                   load_json_fixture('teams/test_team/projects/myprj/get')
 
   describe '#update' do
-    subject { instance.update({}) }
-    it { expect { subject }.to raise_error(NotImplementedError) }
+    let(:request_path) { "/teams/#{owner}/projects/#{project_key}" }
+    before { stub_apiresponse(:put, request_path) }
+    subject { instance.update(name: 'New project name') }
+    it do 
+      expect(subject).to be_an_instance_of(Tinybucket::Model::Project) 
+    end
   end
 
   describe '#destroy' do
-    subject { instance.destroy() }
-    it { expect { subject }.to raise_error(NotImplementedError) }
+    let(:request_path) { "/teams/#{owner}/projects/#{project_key}" }
+    before { stub_apiresponse(:delete, request_path) }
+    subject { instance.destroy }
+    it { expect(subject).to be_nil }
   end
 
   describe '#repos' do
