a1c878dc15e86863f0913bf96b82b7314872decd
José María López Palomo <jose@iJose13.local>
2019-01-23 10:45:01 +0100

    Repos api modified to work alternatively with the list of public repos or account repos depending on owner parameter

--- lib/tinybucket/api/repos_api.rb
+++ lib/tinybucket/api/repos_api.rb
@@ -6,20 +6,20 @@ module Tinybucket
     class ReposApi < BaseApi
       include Tinybucket::Api::Helper::ReposHelper
 
-      # Send 'GET a list of repositories for an account' request
+      # Send 'GET a list of repositories for an account' request or public ones if no account specified
       #
       # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories
-      #   GET a list of repositories for an account
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D
       #
-      # @param options [Hash]
+      #   GET a list of repositories for an account or public ones if none specified
+      #
+      # @param owner [String] The name of the account
+      # @param options [Hash] Configuration options for the request
       # @return [Tinybucket::Model::Page]
-      def list(options = {})
-        opts = options.clone
-        opts.delete(:owner)
-
+      def list(owner=nil, options = {})
         get_path(
-          path_to_list(options),
-          opts,
+          path_to_list(owner, options),
+          options,
           get_parser(:collection, Tinybucket::Model::Repository)
         )
       end
