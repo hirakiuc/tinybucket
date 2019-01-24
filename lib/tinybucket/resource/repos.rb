cc15d389ae346f4a67e23704040e3e1209cc2dad
José María López Palomo <jose@iJose13.local>
2019-01-24 09:51:55 +0100

    Find and create actions implemented for repos resource with tests

--- lib/tinybucket/resource/repos.rb
+++ lib/tinybucket/resource/repos.rb
@@ -8,16 +8,40 @@ module Tinybucket
         @args = [options]
       end
 
+      # List repositories (public ones if no owner provided, owned ones otherwise)
+      #
+      # @param options [Hash] Configuration options
+      # @return [Tinybucket::Model::Page] Paginated repositories
       def list(options = {})
-        repos_api.list(@owner, options)
+        options[:owner] = @owner if @owner.present?
+        repos_api.list(options)
       end
 
-      def create(_options)
-        raise NotImplementedError
+      # Finds the repository defined by the given owner and slug
+      #
+      # @param repo_slug [String] The repository slug identifier
+      # @param options [Hash]
+      # @return [Tinybucket::Model::Repository, NilClass] The identified repository if present and accessible
+      def find(repo_slug, options = {})
+        return nil unless @owner.present? and repo_slug.present?
+        repos_api.find(@owner, repo_slug, options)
       end
 
-      def find(_options)
-        raise NotImplementedError
+      # Creates a new repository with the attributes provided
+      #
+      # @param repo_slug [String] The repository slug identifier
+      # @param options [Hash] Repository attributes
+      # @return [Tinybucket::Model::Repository] The created repository
+      def create(repo_slug, options = {})
+        unless @owner.present?
+          raise ArgumentError,
+                'must provide an owner in order to create a repository'
+        end
+        unless repo_slug.present?
+          raise ArgumentError,
+                'must provide a repo slug in order to create a repository'
+        end
+        repos_api.create(@owner, repo_slug, options)
       end
 
       private
