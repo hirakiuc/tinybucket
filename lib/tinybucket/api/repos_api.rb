64689504fed302614674ca748e071cfaec426315
José María López Palomo <jose@iJose13.local>
2019-01-24 09:52:24 +0100

    Find and create actions created for repos api with tests

--- lib/tinybucket/api/repos_api.rb
+++ lib/tinybucket/api/repos_api.rb
@@ -5,22 +5,53 @@ module Tinybucket
     # Repos Api client
     class ReposApi < BaseApi
       include Tinybucket::Api::Helper::ReposHelper
-
-      # Send 'GET a list of repositories for an account' request or public ones if no account specified
+    
       #
-      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories
-      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D
+      # GET a list of repositories for an account or public ones if none specified
       #
-      #   GET a list of repositories for an account or public ones if none specified
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories#get
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D#get
       #
       # @param owner [String] The name of the account
       # @param options [Hash] Configuration options for the request
       # @return [Tinybucket::Model::Page]
-      def list(owner = nil, options = {})
+      def list(options = {})
         get_path(
-          path_to_list(owner, options),
+          path_to_list(options),
           options,
           get_parser(:collection, Tinybucket::Model::Repository)
+        )
+      end
+
+      #
+      # GET the identified repository for the given account, nil otherwise
+      #
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D#get
+      #
+      # @param owner [String] The name of the account
+      # @param options [Hash] Configuration options for the request
+      # @return [Tinybucket::Model::Page]
+      def find(repo_owner, repo_slug, options = {})
+        get_path(
+          path_to_find(repo_owner, repo_slug),
+          options,
+          get_parser(:object, Tinybucket::Model::Repository)
+        )
+      end
+
+      #
+      # POST (create) a new repository with the provided information for the given account
+      #
+      # @see https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D#post
+      #
+      # @param owner [String] The name of the account
+      # @param options [Hash] Configuration options for the request
+      # @return [Tinybucket::Model::Page]
+      def create(repo_owner, repo_slug, options = {})
+        post_path(
+          path_to_post(repo_owner, repo_slug),
+          options,
+          get_parser(:object, Tinybucket::Model::Repository)
         )
       end
     end
