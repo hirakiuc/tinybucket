89bebdf2b7a0777954f8ee582af433cfda77e7f4
José María López Palomo <jose@iJose13.local>
2019-01-24 09:52:42 +0100

    Repos helper updated to include path_to find and post actions

--- lib/tinybucket/api/helper/repos_helper.rb
+++ lib/tinybucket/api/helper/repos_helper.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 module Tinybucket
   module Api
     module Helper
@@ -8,9 +7,18 @@ module Tinybucket
 
         private
 
-        def path_to_list(owner = nil, _options = {})
+        def path_to_list(options = {})
+          owner = options.delete(:owner)
           return base_path if owner.blank?
           build_path(base_path, [owner, 'owner'])
+        end
+
+        def path_to_find(repo_owner, repo_slug)
+          build_path(base_path, [repo_owner, 'owner'], [repo_slug, 'slug'])
+        end
+
+        def path_to_post(repo_owner, repo_slug)
+          build_path(base_path, [repo_owner, 'owner'], [repo_slug, 'slug'])
         end
 
         def base_path
