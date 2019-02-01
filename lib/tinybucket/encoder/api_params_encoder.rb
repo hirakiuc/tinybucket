c9ee41688a8b9ba0a41d65e2f7d0bdaca06ddc45
José María López Palomo <jose@iJose13.local>
2019-02-01 18:51:03 +0100

    The ApiParamsEncoder has been documented

--- lib/tinybucket/encoder/api_params_encoder.rb
+++ lib/tinybucket/encoder/api_params_encoder.rb
@@ -1,6 +1,11 @@
 module Tinybucket
   module Encoder
     class ApiParamsEncoder
+      #
+      # Encode the given params in the way the API expect them
+      #
+      # @param [Hash] params The params we want to encode
+      # @return [String] The params encoded ready to be included in URL
       def self.encode(params = {})
         return nil if params.nil?
         params = sort_parameters(params)
@@ -10,6 +15,11 @@ module Tinybucket
         [params_encoded, encoded_query_params].compact.join('&')
       end
 
+      #
+      # Decode the URL string containing the included parameters
+      #
+      # @param [String] query The url part including the encoded parameters
+      # @return [Hash] The parameters extracted from the URL string
       def self.decode(query)
         return nil if query.nil?
         decoded_params = Faraday::FlatParamsEncoder.decode(query)
@@ -18,10 +28,21 @@ module Tinybucket
         decoded_params.merge(decoded_query_params).symbolize_keys
       end
 
+      #
+      # Sort the given prameters by key
+      #
+      # @param [Hash] parameters The parameters hash we want to sort
+      # @return [Hash] The sorted parameters
       def self.sort_parameters(parameters)
         parameters.sort_by { |key, _value| key }.to_h
       end
+      private_class_method :sort_parameters
 
+      #
+      # Encode the parameters corresponding to a query
+      #
+      # @param [Hash] query_parameters  The query parameters we want to encode
+      # @return [String] The already encoded query parameters
       def self.encode_query_parameters(query_parameters)
         formatted_parameters = query_parameters.map do |key, value|
           while value.is_a?(Hash)
@@ -37,6 +58,11 @@ module Tinybucket
       end
       private_class_method :encode_query_parameters
 
+      #
+      # Decode the parameters corresponding to a query
+      #
+      # @param [String] encoded_parameters The string containing encoded query parameters
+      # @return [Hash] The already decoded query parameters
       def self.decode_query_parameters(encoded_parameters)
         decoded_parameters = Faraday::FlatParamsEncoder.decode(encoded_parameters)
         formatted_parameters = decoded_parameters.map do |key, value|
@@ -54,6 +80,11 @@ module Tinybucket
       end
       private_class_method :decode_query_parameters
 
+      #
+      # Encode a query parameter value
+      #
+      # @param [any] value The query parameter value we want to encode
+      # @return [String] The encoded query parameter value
       def self.encode_query_value(value)
         if value.is_a? String
           "\"#{value}\""
@@ -65,6 +96,11 @@ module Tinybucket
       end
       private_class_method :encode_query_value
 
+      #
+      # Decode an encoded query parameter value
+      #
+      # @param [String] value The encoded query parameter value we want to decode
+      # @return [any] The interpreted and decoded query parameter value
       def self.decode_query_value(value)
         case value
         when /^".*"$/
